6475baa0d29445577b54d0ffd1497843e02b5e8e
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2021-08-18 02:40:57 +0530

    updated build command and added firebase config to prod environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAIbpaE-laye99kqwOGh0quT-RCrIl27-g',
+    authDomain: 'nit-time-table.firebaseapp.com',
+    projectId: 'nit-time-table',
+    storageBucket: 'nit-time-table.appspot.com',
+    messagingSenderId: '1028567117876',
+    appId: '1:1028567117876:web:e1a57f9a8353cec777da61',
+    measurementId: 'G-Y9RKLZYSWX',
+  },
 };
