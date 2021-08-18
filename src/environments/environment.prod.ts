72b1a64902d9eb55ae832068c0b15a3947c8689a
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2021-08-19 00:59:43 +0530

    prod enviroment file error fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://fathomless-refuge-76233.herokuapp.com',
-  // baseUrl: 'http://localhost:4000',
   firebaseConfig: {
     apiKey: 'AIzaSyAIbpaE-laye99kqwOGh0quT-RCrIl27-g',
     authDomain: 'nit-time-table.firebaseapp.com',
@@ -12,5 +11,3 @@ export const environment = {
     measurementId: 'G-Y9RKLZYSWX',
   },
 };
-{"_id":{"$oid":"611d02b09710d80016bd89ff"},"fcmToken":"c9olGUhNqWMJmEdxERGNz4:APA91bFu2M0Afxrm36RkcKfiHpmpTicFhv-icIR00lGiVEC9bDNox7XaStsz-TYixKbNuYheeC8AchhCU9rv3O5nP_C4H2kTfve_w4kkzZSv8ZQrAk7ngXZOQAdtX6b4SUbe7UlgD3lm","createdAt":{"$date":{"$numberLong":"1629291184124"}},"updatedAt":{"$date":{"$numberLong":"1629291184124"}},"__v":{"$numberInt":"0"}}
-{"_id":{"$oid":"611d03369710d80016bd8a03"},"fcmToken":"diCwEt7pVQBJ2CU4Egtkum:APA91bF11kjjuYHjoCY6fdnIeYqNudmd3FTaSBkiMgpgwfOzYHnvi7lNd2Wsg1RQfj92hACBDgnbfvYt6ca7c96kPx6DumeKZj2JJXZZYAyCwoMcR_pn4BzF-8saVhdYDc9jLZ5n5fFb","createdAt":{"$date":{"$numberLong":"1629291318006"}},"updatedAt":{"$date":{"$numberLong":"1629291318006"}},"__v":{"$numberInt":"0"}}
