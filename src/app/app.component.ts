b2b18b6a649ec69d232990b7735b625a1eb909b3
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2021-08-18 16:00:30 +0530

    added service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { MatSelect } from '@angular/material/select';
 import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'angular-frontend';
+
+  @ViewChild('sem_select')
+  semSelector!: MatSelect;
+
+  @ViewChild('branch_select')
+  branchSelector!: MatSelect;
+
+  editSem: boolean = false;
+
+  editBranch: boolean = false;
+  sems = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th'];
+
+  editSemester() {
+    this.editSem = true;
+    // this.semSelector.open();
+    console.log(this.semSelector);
+  }
+
+  setBranch() {
+    this.editBranch = true;
+    // this.branchSelector.open();
+    // console.log(this.semSelector);
+  }
+
+  branchs = [
+    'Computer',
+    'Civil',
+    'Information Techology',
+    'Mechanical',
+    'Electrical',
+    'Electronics & Communication',
+  ];
+
+  sem: string = this.sems[0];
+
+  branch: string = this.branchs[0];
 
   constructor(public authService: AngularFireAuth) {}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,17 @@ import { MinutesTimePipe } from './minutes-time.pipe';
 import { PeriodBlockComponent } from './period-block/period-block.component';
 
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
 import {
   MatSnackBarModule,
   MAT_SNACK_BAR_DEFAULT_OPTIONS,
 } from '@angular/material/snack-bar';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -37,9 +42,17 @@ import { environment } from 'src/environments/environment';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatDialogModule,
     MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
+    ServiceWorkerModule.register('firebase-messaging-sw.js', {
+      enabled: true,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [
     {
@@ -47,7 +60,7 @@ import { environment } from 'src/environments/environment';
       useValue: {
         duration: 2500,
         horizontalPosition: 'left',
-        panelClass: ['bg-gray-900'],
+        panelClass: ['bg-gray-100', 'shadow-lg', 'text-black', 'border-2'],
       },
     },
   ],
