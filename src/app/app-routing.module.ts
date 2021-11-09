dbace3b1f6c07d82dff48613b71770debef06831
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2021-11-10 01:42:33 +0530

    add google login and firestore rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddPeriodComponent } from './add-period/add-period.component';
 import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './auth.guard';
 import { BranchSemOptionsResolver } from './branch-sem-options.resolver';
 import { BranchSemSelectedGuard } from './branch-sem-selected.guard';
 import { ProfileComponent } from './profile/profile.component';
@@ -16,10 +17,12 @@ const routes: Routes = [
   {
     path: 'add-period',
     component: AddPeriodComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'admin',
     component: AdminComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'profile',
